032bd9132431428704e78ac000d9a7ff0bf5ad9d
nawelbarhoumi <nawel.barhoumi@gmail.com>
2021-02-04 15:04:33 +0100

    commit product

--- js/product.js
+++ js/product.js
@@ -1,12 +1,28 @@
-function add(){
-    let name = document.getElementById('');
-    let price = document.getElementById('');
-    var storage = JSON.parse(localStorage.getItem('photos')) || [];
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
+async function add(){
+    let name = document.getElementById('name');
+    let price = document.getElementById('price');
+    let picture = document.getElementById('picture');
+
+    //
+    let base64 = "";
+    if(picture.files.length>0){
+        const image = picture.files[0];
+        base64 = await toBase64(image);    
+    }
+    var storage = JSON.parse(localStorage.getItem('products')) || [];
     storage.push(
         {
             name: name.value,
-            price: price.value
+            price: price.value,
+            image: base64
         })
-    localStorage.setItem('photos', JSON.stringify(storage));
+    localStorage.setItem('products', JSON.stringify(storage));
 
 }
