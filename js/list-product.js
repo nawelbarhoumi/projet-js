7ccf47fba50e95d464ff159cfc16a66a7c81a06d
AymenBouazra <aymenbouazra994@gmail.com>
2021-02-05 17:10:44 +0100

    changement logo et creation de edit product

--- js/list-product.js
+++ js/list-product.js
@@ -4,21 +4,22 @@ function storageonload(){
     var produits=''
     storage.forEach((x) =>  {   
        produits+= `
-       <div class="col-md-4 grid-product-in">	
-       <div class=" product-grid">	
-           <a href="single.html"><img class="img-responsive " src="${x.image}" alt=""></a>		
-           <div class="shoe-in">
-               <h6><a href="single.html">
+        <div class="col-md-4 grid-product-in">	
+        <div class=" product-grid">	
+            <a href="single.html"><img class="img-responsive " src="${x.image}" alt=""></a>		
+            <div class="shoe-in">
+                <h6><a href="single.html">
                   ${x.name}
-               <label>${x.price}</label>
+                <label>${x.price}</label>
                <a href="single.html" class="store">FIND A STORE</a>
-           </div>
-           
-           <b class="plus-on">+</b>
-       </div>	
-       </div>
-`
-
+            </div>
+            <a href="edit-product.html">Update</a>
+            
+          </button>
+            <b class="plus-on"><a><i class="fas fa-edit" style="color: black !important"></i></a></b>
+        </div>	
+        </div>
+    `
 });
 
 
